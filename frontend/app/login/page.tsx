33b3be942cbe0c45ce1c9d8ef2def4bc0a64f544
felipeab-b <felipeabferreira22@gmail.com>
2025-10-22 22:05:41 -0300

    style: corrigindo erros no tailwind, importando fontes e consertando o espaçamento

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -3,46 +3,41 @@ import Image from "next/image";
 export default function LoginPage() {
   return (
     <main
-      className="min-h-screen flex items-center justify-center"
-      style={{ backgroundColor: "#f5f5f5" }}
+      className="bg-background min-h-screen flex items-center justify-center"
     >
-      <div className="flex flex-row items-center justify-center w-4/5 gap-10">
+      <div className="flex flex-row items-center justify-center w-full max-w-6xl mx-auto gap-15">
         
         {/* Imagem */}
         <div className="flex-1 flex justify-center">
           <Image
             src="/images/login-image.svg"
             alt="Login Image"
-            width={500} 
-            height={500}
+            width={700} 
+            height={700}
             className="object-contain"
           />
         </div>
 
         {/* Card */}
         <div
-          className="flex-1 flex flex-col p-10 rounded-lg shadow-lg max-w-md text-center"
-          style={{ backgroundColor: "#e0e0e0" }} 
+          className="bg-card flex-1 flex flex-col p-10 py-20 rounded-lg shadow-lg max-w-md text-center"
         >
           <h1
-            className="font-bold text-2xl tracking-wider mb-6"
-            style={{ color: "#ff6700" }} 
+            className="text-laranja font-sans font-extrabold text-2xl tracking-wider mb-6"
           >
             BEM VINDO DE VOLTA!
           </h1>
 
           <div className="flex flex-col gap-3 my-2 text-gray-800 mb-3">
             <input
-              style={{ backgroundColor: "#f5f5f5" }}
-              className="rounded-full p-2 border border-gray-300"
+              className="bg-background rounded-full p-2 pl-4 border border-gray-300"
               type="text"
               name="Email"
               id="email"
               placeholder="Email"
             />
             <input
-              style={{ backgroundColor: "#f5f5f5" }}
-              className="rounded-full p-2 border border-gray-300"
+              className="bg-background rounded-full p-2 pl-4 border border-gray-300"
               type="password"
               name="Senha"
               id="pass"
@@ -50,23 +45,23 @@ export default function LoginPage() {
             />
           </div>
 
-          <p><a
-            style={{ color: "#ff6700" }}
-            className=" mb-5 hover:underline"
+          <p
+            className="mb-5"
+          ><a
+            className="text-laranja hover:underline"
             href=""
           >
             Esqueceu a senha?
           </a></p>
 
           <button
-            style={{ backgroundColor: "#ff6700", color: "#ffffff" }}
-            className="rounded-full mt-6 p-2 hover:brightness-90 transition cursor-pointer"
+            className="bg-laranja text-white font-sans tracking-wider text-xl rounded-full mt-6 p-3 hover:brightness-90 transition cursor-pointer"
           >
             Entrar
           </button>
 
-          <p className="mt-2 text-gray-700">
-            Não possui conta? <a href="" className="text-orange-500 hover:underline">Crie Aqui!</a>
+          <p className="mt-3 text-gray-700">
+            Não possui conta? <a href="/cadastro" className="text-orange-500 hover:underline">Crie Aqui!</a>
           </p>
         </div>
 
